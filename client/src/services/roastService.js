7b1b47d0c0ebe152c2b8e0f494759a5badd07d59
Akalya <156876691+Akalya2005p@users.noreply.github.com>
2026-06-25 14:10:17 +0530

    Update roastService.js

--- client/src/services/roastService.js
+++ client/src/services/roastService.js
@@ -1,13 +1,13 @@
 import axios from "axios";
 
-const API_URL = "http://localhost:5000/api/roast";
+const API_URL = import.meta.env.VITE_API_URL;
 
 export const roastUrl = async (url) => {
-  const response = await axios.post(API_URL, { url });
+  const response = await axios.post(`${API_URL}/api/roast`, { url });
   return response.data;
 };
 
 export const getRecentRoasts = async () => {
-  const response = await axios.get(`${API_URL}/recent`);
+  const response = await axios.get(`${API_URL}/api/roast/recent`);
   return response.data;
 };
